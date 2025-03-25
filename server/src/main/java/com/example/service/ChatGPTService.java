c7c29d78e73f8f15359c316af5af860dc8b9ee14
GihaniWijekoon <124669194+GihaniWijekoon@users.noreply.github.com>
2025-03-25 12:41:29 +0530

    aaa

--- server/src/main/java/com/example/service/ChatGPTService.java
+++ server/src/main/java/com/example/service/ChatGPTService.java
@@ -4,7 +4,6 @@ import com.example.controller.RequestController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
